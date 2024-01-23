a1b442da67031be5bfd32f7be811b4be6b3f23e5
Philip <1611.developer@gmail.com>
2024-01-22 19:10:35 -0800

    image display work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+const imageUrl = "https://random.dog/woof.json"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  
+  const [photo, setPhoto] = useState()
+  
+  async function getPhoto() {
+    //const res = await fetch(imageUrl)
+    //const imageBlob = await res.blob();
+    //const imageObjectURL = URL.createObjectURL(imageBlob);
+    //setPhoto(imageObjectURL.url)
+    const res = await fetch('https://random.dog/woof.json')
+    const data = await res.json()
+    console.log(url.data.photo)
+    setPhoto(data.photo)
+  }
+
+  useEffect(function() {
+    getPhoto()
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <img src={photo} />
+        </div>
     </>
   )
 }
 
-export default App
