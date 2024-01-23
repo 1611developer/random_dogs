2efd03102a6a44f50d63284b649aa52a12018953
Philip <1611.developer@gmail.com>
2024-01-23 12:34:20 -0800

    still working on displaying images

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-const imageUrl = "https://random.dog/woof.json"
 
 export default function App() {
-  
-  const [photo, setPhoto] = useState()
-  
+  const [photo, setPhoto] = useState("")
   async function getPhoto() {
-    //const res = await fetch(imageUrl)
-    //const imageBlob = await res.blob();
-    //const imageObjectURL = URL.createObjectURL(imageBlob);
-    //setPhoto(imageObjectURL.url)
     const res = await fetch('https://random.dog/woof.json')
     const data = await res.json()
-    console.log(url.data.photo)
-    setPhoto(data.photo)
+    console.log(data)
+    setPhoto(data.Photo)
   }
 
   useEffect(function() {
-    getPhoto()
-  },[])
+    getActivity()
+  }
 
   return (
     <>
       <div className="card">
-          <img src={photo} />
-        </div>
+      </div>
     </>
   )
 }
 
+export default App
